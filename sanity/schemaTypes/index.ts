728dfaebf8d52237e7522597592029d7afe74704
Caitlin <c_smith24@mail.fhsu.edu>
2026-05-02 18:20:19 -0500

    Forgot to register themeVerse schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity';
 import { sermon } from './sermon';
 import { event } from './event';
 import prayerRequest from './prayerRequest';
+import themeVerse from './themeVerse';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [sermon, event, prayerRequest],
+  types: [sermon, event, prayerRequest, themeVerse],
 };
